d6cc524391fbf649f7666921705110dfff2c8250
bugu <1759743261@qq.com>
2022-12-13 16:18:37 +0800

    测试 git 修改

--- src/main/java/com/bugu/Application.java
+++ src/main/java/com/bugu/Application.java
@@ -11,6 +11,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 public class Application {
 
     public static void main(String[] args) {
+        System.out.println("=============================");
         SpringApplication.run(Application.class, args);
     }
 
